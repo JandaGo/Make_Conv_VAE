d2f6ba3b536a0a910da2b81d7aae52bf01e7903a
JandaGo <qkrwogks7094@gmail.com>
2022-05-26 10:47:05 +0900

    make_base

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Make_Conv_VAE
+
+## Used Dataset
+
+Not in the Repository
+
+DATASET_PCB
+    train
+        train_split
+            .jpg
+
+    test
+        crop
+            Missing_hole
+                .jpg
+
+
+
+
+
+    
---
+++ split.py
@@ -0,0 +1,31 @@
+from PIL import Image
+from tqdm import tqdm
+import glob
+import cv2
+import numpy as np
+# filename_list = glob.glob('./01.jpg')
+filename_list = glob.glob('./*.jpg')
+filename_list.sort()
+ 
+fill_number = len(str(len(filename_list)))
+a = 0
+# print(fill_number)
+for idx, filename in enumerate(tqdm(filename_list), 1):
+    im = Image.open(filename)
+
+    # cv2.imshow('window',im)
+    # cv2.waitKey(0)
+
+    print(np.shape(im))
+    print(a)
+    # i = 0
+    h, w, c = np.shape(im)
+    for i in range(1,5):
+        for j in range(1,5):
+            area = ((i-1)*(1/4)*w, (j-1)*(1/4)*h, i*(1/4)*w, j*(1/4)*h)
+            crop_image = im.crop(area)
+            savename = './save/crop_' +str(a)+str(i)+str(j)+ '.jpg'
+            # savename = './save/crop_' +str(i)+ str(j)+str(idx).zfill(fill_number) + '.jpg'
+
+            crop_image.save(savename)
+    a+=1
